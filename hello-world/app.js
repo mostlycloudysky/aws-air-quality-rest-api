70b30f45fed76e1fe926816b5114e599c691fbb0
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-10-22 10:13:49 -0400

    sam changes to read secret manager values using aws-sdk and template updates

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,4 +1,9 @@
 const axios = require('axios')
+const aws = require('aws-sdk')
+const client = new aws.SecretsManager({
+    region: 'us-east-2'
+});
+let secret, decodedBinarySecret;
 const url = 'http://checkip.amazonaws.com/';
 let response;
 
@@ -15,13 +20,31 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
+
+    client.getSecretValue({
+        SecretId: 'samplesecret'
+    }, function(err, data) {
+        if (err) {
+            throw err
+        } else {
+            if ('SecretString' in data) {
+                secret = data.SecretString;
+            } else {
+                let buff = new Buffer(data.SecretBinary, 'base64');
+                decodedBinarySecret = buff.toString('ascii');
+            }
+        }
+    })
+
     try {
         const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'hello world',
-                location: ret.data.trim()
+                location: ret.data.trim(),
+                test: 'test',
+                code: JSON.parse(secret)
             })
         }
     } catch (err) {
